aa040d463203937a132b380f586a952da3113a19
Niraj Pandey <nirajpandey034@gmail.com>
2021-06-12 13:39:49 +0530

    nothing major

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import firebase from '../firebase'
 import Cards from './Cards'
 
@@ -13,50 +13,51 @@ import Tooltip from '@material-ui/core/Tooltip';
 
 
 function Board() {
-    const [data, setData] = useState([])  
+    const [data, setData] = useState([])
     const [flag, setFlag] = useState(true)
 
-    useEffect(()=>{
-        const getData = () =>{
+    useEffect(() => {
+        const getData = () => {
             let dat = [];
-            firebase.database().ref("codeBase").on("child_added", (snapshot, prevChildKey) =>{    
-                let value = snapshot.val(); 
-                dat = [...dat ,<Cards key={value.code_title} owner_name={value.owner_name} owner_email={value.owner_email} 
+            firebase.database().ref("codeBase").on("child_added", (snapshot, prevChildKey) => {
+                let value = snapshot.val();
+                dat = [...dat, <Cards key={value.code_title} owner_name={value.owner_name} owner_email={value.owner_email}
                     code_title={value.code_title} code_url={value.code_url}
-                    code_approach={value.code_approach} code_text={value.code_text}/>]
+                    code_approach={value.code_approach} code_text={value.code_text} />]
                 setFlag(false)
-        });
-        setData(dat);
+            });
+            setData(dat);
         }
-            getData(); 
-    },[flag])
-    return (       
+        getData();
+    }, [flag])
+    return (
         <div>
             <Tooltip title="Created by Niraj Pandey">
-            <h1 
-            style={{textDecoration:'none', backgroundColor:'black', color:'white',cursor:'default'
-             }}>The CODE BASE</h1>
+                <h1
+                    style={{
+                        textDecoration: 'none', backgroundColor: 'black', color: 'white', cursor: 'default'
+                    }}>The CODE BASE</h1>
             </Tooltip>
 
-                {(data.length === 0) ? <CircularProgress /> : data}
+            {(data.length === 0) ? <CircularProgress /> : data}
 
             <br />
             <Tooltip title='Upload Your Code'>
-            <a href="https://ur-code-base.web.app/" target="_blank" rel="noreferrer"><AddCircleRoundedIcon fontSize="large" color="primary"/></a>
+                <a href="https://ur-code-base.web.app/" target="_blank" rel="noreferrer"><AddCircleRoundedIcon fontSize="large" color="primary" /></a>
             </Tooltip>
             &emsp;&emsp;
             <Tooltip title='Get in touch with Creator'>
-            <a href="https://www.linkedin.com/in/imnirajpandey" target="_blank" rel="noreferrer"><EmojiPeopleIcon fontSize="large" color="primary"/></a>
+                <a href="https://www.linkedin.com/in/imnirajpandey" target="_blank" rel="noreferrer"><EmojiPeopleIcon fontSize="large" color="primary" /></a>
             </Tooltip>
             &emsp;&emsp;
             <Tooltip title='Github Repository'>
-            <a href="https://github.com/nirajpandey034/niraj_CodeBase" target="_blank" rel="noreferrer"><CodeRoundedIcon fontSize="large" color="primary"/></a>
+                <a href="https://github.com/nirajpandey034/niraj_CodeBase" target="_blank" rel="noreferrer"><CodeRoundedIcon fontSize="large" color="primary" /></a>
             </Tooltip>
             &emsp;&emsp;
             <Tooltip title='Share'>
-            <a href="https://facebook.com/" target="_blank" rel="noreferrer"><FacebookIcon fontSize="large" color="primary"/></a>
+                <a href="https://facebook.com/" target="_blank" rel="noreferrer"><FacebookIcon fontSize="large" color="primary" /></a>
             </Tooltip>
         </div>
     )
 }
- export default Board
+export default Board
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+
 import Card from '@material-ui/core/Card';
 import { makeStyles } from '@material-ui/core/styles';
 import CardActions from '@material-ui/core/CardActions';
@@ -8,7 +9,6 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import EmailIcon from '@material-ui/icons/Email';
 import CodeIcon from '@material-ui/icons/Code';
-
 import Dialog from '@material-ui/core/Dialog';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItem from '@material-ui/core/ListItem';
@@ -19,7 +19,6 @@ import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Slide from '@material-ui/core/Slide';
-
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
@@ -28,6 +27,8 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import emailjs from 'emailjs-com';
 import Tooltip from '@material-ui/core/Tooltip';
 
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import ThumbDownIcon from '@material-ui/icons/ThumbDown';
 
 
 const Transition = React.forwardRef(function Transition(props, ref) {
@@ -35,148 +36,151 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 });
 //Coded by Niraj Pandey
 function Cards(props) {
-    const useStyles = makeStyles((theme)=>({
-        root: {
-          maxWidth: 1000,
-          margin: '20px',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          overflow: 'auto',
-          backgroundColor:'#d5f4e6',
-          zIndex:10,
-        },
-        bullet: {
-          display: 'inline-block',
-          margin: '0 2px',
-          transform: 'scale(0.8)',
-        },
-        owner_name: {
-            width:'auto',
-            textAlign:'left',
-            fontSize: 12,
-            display:'inline-block',
-        },
-        owner_email: {
-            width:'auto',
-            textAlign:'right',
-            fontSize: 12,
-            display:'inline-block',
-        },
-        pos: {
-          marginBottom: 12,
-        },
-        btn: {
-            borderRadius: '4px',
-            fontFamily: "Arial",
-        },
-        modal: {
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            overflow:'auto',
-        },
-        paper: {
-            backgroundColor: theme.palette.background.paper,
-            border: '2px solid #000',
-            boxShadow: theme.shadows[5],
-            padding: theme.spacing(2, 4, 3),
-        },
-        appBar: {
-          position: 'relative',
-          backgroundColor:'#d5f4e6',
-          color:'black',
-        },
-        title: {
-          marginLeft: theme.spacing(2),
-          flex: 1,
-        },
-      }));
-    const classes = useStyles();
-    const [open, setOpen] = useState(false);
-    const [ShareOpen, setShareOpen] = useState(false);
-    const [email, setEmail] = useState('');
-    //sent status 
-    const [sending, setSending] = useState('')
-    const bull = <span className={classes.bullet}>•</span>;
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      maxWidth: 1000,
+      margin: '20px',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      overflow: 'auto',
+      backgroundColor: '#d5f4e6',
+      zIndex: 10,
+    },
+    bullet: {
+      display: 'inline-block',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
+    owner_name: {
+      width: 'auto',
+      textAlign: 'left',
+      fontSize: 12,
+      display: 'inline-block',
+    },
+    owner_email: {
+      width: 'auto',
+      textAlign: 'right',
+      fontSize: 12,
+      display: 'inline-block',
+    },
+    pos: {
+      marginBottom: 12,
+    },
+    btn: {
+      borderRadius: '4px',
+      fontFamily: "Arial",
+    },
+    modal: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      overflow: 'auto',
+    },
+    paper: {
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+    appBar: {
+      position: 'relative',
+      backgroundColor: '#d5f4e6',
+      color: 'black',
+    },
+    title: {
+      marginLeft: theme.spacing(2),
+      flex: 1,
+    },
+  }));
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [ShareOpen, setShareOpen] = useState(false);
+  const [email, setEmail] = useState('');
+  const [sending, setSending] = useState('')
 
-    const handleOpen = () => {
-        setOpen(true);
-      };
-    
-      const handleClose = () => {
-        setOpen(false);
-      };
+  const bull = <span className={classes.bullet}>•</span>;
 
-      const handleShareOpen = () => {
-        setShareOpen(true);
-      };
-    
-      const handleShareClose = () => {
-        setShareOpen(false);
-      };
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-      const setemail = (e) =>{
-          setEmail(e.target.value)
-      }
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-      const sendEmail = () =>{
-        let data1 = {
-          from_name: 'CodeBase',
-          to_email: email,
-          message: props.code_text,
-          reply_to:'codewithniraj034@gmail.com'
-      };
-      setSending('');
-        emailjs.send('service_codebase', 'template_t4z1v0s', data1, 'user_etDRuxhbEwmGrJ7URIpL1')
-        .then(()=>{alert('Sent');  setEmail(''); setSending(''); handleShareClose();})
-        .catch((err)=>{setSending('Error Occured');})
-      }
-    return (
-        <div>
-          <Card className={classes.root}>
-            <CardContent>
-                    <Typography className={classes.owner_name} color="textSecondary" 
-                    component='p'> 
-                        <strong>Contributed By:</strong> {props.owner_name}</Typography>
-                    {bull}
-                    <Typography className={classes.owner_email} color="secondary" 
-                    style={{cursor : 'pointer', textDecoration: 'underline'}}
-                    component='p' onClick={()=>{window.location.href='mailto:' + props.owner_email}}> 
-                    {props.owner_email}</Typography>
+  const handleShareOpen = () => {
+    setShareOpen(true);
+  };
+
+  const handleShareClose = () => {
+    setShareOpen(false);
+  };
+
+  const setemail = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const sendEmail = () => {
+    let data1 = {
+      from_name: 'CodeBase',
+      to_email: email,
+      message: props.code_text,
+      reply_to: 'codewithniraj034@gmail.com'
+    };
+    setSending('');
+    emailjs.send('service_codebase', 'template_t4z1v0s', data1, 'user_etDRuxhbEwmGrJ7URIpL1')
+      .then(() => { alert('Sent'); setEmail(''); setSending(''); handleShareClose(); })
+      .catch((err) => { setSending('Error Occured'); })
+  }
+  return (
+    <div>
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography className={classes.owner_name} color="textSecondary"
+            component='p'>
+            <strong>Contributed By:</strong> {props.owner_name}</Typography>
+          {bull}
+          <Typography className={classes.owner_email} color="secondary"
+            style={{ cursor: 'pointer', textDecoration: 'underline' }}
+            component='p' onClick={() => { window.location.href = 'mailto:' + props.owner_email }}>
+            {props.owner_email}</Typography>
 
 
-                    <Typography  color="textPrimary" variant='h5'
-                    style={{marginTop:'6px'}}> 
-                    {props.code_title}</Typography>
+          <Typography color="textPrimary" variant='h5'
+            style={{ marginTop: '6px' }}>
+            {props.code_title}</Typography>
 
-                    <Typography  color="primary" variant='overline' 
-                    onClick={()=>{window.open(props.code_url)}} 
-                    style={{cursor : 'pointer', textDecoration: 'underline'}}> 
-                    {props.code_url}
-                    </Typography>
-            </CardContent>
-            <CardActions>
-              <div style={{display:'flex', width:'100%'}}>
-                <div>
-                <Tooltip title='Approach & Code'>
-                <CodeIcon size="small" className="btn" 
-                color="secondary" onClick={handleOpen} >
+          <Typography color="primary" variant='overline'
+            onClick={() => { window.open(props.code_url) }}
+            style={{ cursor: 'pointer', textDecoration: 'underline' }}>
+            {props.code_url}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <div style={{ display: 'flex', width: '100%' }}>
+            <div style={{ marginRight: 'auto' }}>
+              <Tooltip title='Approach & Code'>
+                <CodeIcon size="small" className="btn"
+                  color="secondary" onClick={handleOpen} >
                 </CodeIcon>
-                </Tooltip>
-                </div>
-             
-                <div style={{marginLeft:'auto', marginRight:'0'}}>
-                <Tooltip title='Send this to your email'>
+              </Tooltip>
+            </div>
+            <div style={{ marginLeft: 'auto', marginRight: '0' }}>
+              <Tooltip title='Send this to your email'>
                 <EmailIcon size="small"
-                color="secondary" onClick={handleShareOpen} >
+                  color="secondary" onClick={handleShareOpen} >
                 </EmailIcon>
-                </Tooltip>
-                </div>
-              </div>
-            </CardActions>
-        </Card>
-        {/* Code section */}
-            <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
+              </Tooltip>
+            </div>
+          </div>
+        </CardActions>
+      </Card>
+      {/* Code section */}
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}>
         <AppBar className={classes.appBar}>
           <Toolbar>
             <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
@@ -208,9 +212,9 @@ function Cards(props) {
         aria-describedby="alert-dialog-slide-description"
       >
         <DialogTitle id="alert-dialog-slide-title">{"Share the Code"}</DialogTitle>
-        <DialogContent> 
-          <TextField type="text" placeholder="Enter your Email" onChange={setemail}/>
-          <p style={{color:'red'}}>{sending}</p>
+        <DialogContent>
+          <TextField type="text" placeholder="Enter your Email" onChange={setemail} />
+          <p style={{ color: 'red' }}>{sending}</p>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleShareClose} color="primary">
@@ -221,8 +225,8 @@ function Cards(props) {
           </Button>
         </DialogActions>
       </Dialog>
-        </div>
-    )
+    </div>
+  )
 }
 
 export default Cards
